a060694d150d627c200e151ad78fb74ad4bb4bfa
Gaurav Parihar <52989286+GauravSingh1999@users.noreply.github.com>
2019-10-14 23:18:00 +0530

    Update 1_0_Knapsack.py

--- 1_0_Knapsack.py
+++ 1_0_Knapsack.py
@@ -1,5 +1,5 @@
-def KnapSack(val,wt,maxWeight):
-    n=len(val)
+def KnapSack(value,w,maxWeight):
+    n=len(value)
     i=0
     j=0
     K=[[0 for m in range(maxWeight+1)] for y in range(n+1)]
@@ -9,7 +9,7 @@ def KnapSack(val,wt,maxWeight):
             if( i==0 or j==0):
                 K[i][j]=0
             elif wt[i-1]<=j:
-                K[i][j]=max(val[i-1]+K[i-1][j-wt[i-1]],K[i-1][j])   
+                K[i][j]=max(value[i-1]+K[i-1][j-wt[i-1]],K[i-1][j])   
             else:
                 K[i][j]=K[i-1][j]
     
@@ -17,7 +17,7 @@ def KnapSack(val,wt,maxWeight):
 
 
 wt=[1,3,4,5]
-val=[1,4,5,7]
+value=[1,4,5,7]
 maxWeight=7
-k=KnapSack(val=val,wt=wt,maxWeight=maxWeight)
+k=KnapSack(value=value,wt=wt,maxWeight=maxWeight)
 print(k)
